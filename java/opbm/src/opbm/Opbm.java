61bfdb5562570949d85e711707c9a2f366319402
Rick C. Hodgin <rick@canalabs.com>
2011-09-09 14:25:59 -0500

    For #31 - Scripts.xml input for backing out of error conditions by running atoms when errors are encountered (such as an install script setting up an "uninstall atom" to run if a failure is encountered, logic still needs added to manifest.xml and manifest.xml processor.

--- java/opbm/src/opbm/Opbm.java
+++ java/opbm/src/opbm/Opbm.java
@@ -2908,6 +2908,6 @@ public final class Opbm extends	ModalApp
 
 	// Used for the build-date and time
 //	public final static String		m_version				= "Built 2011.08.22 05:19am";
-	public final static String		m_version				= "-- DEV BRANCH BUILD -- UNSTABLE -- Built 2011.09.09 02:10pm";
+	public final static String		m_version				= "-- DEV BRANCH BUILD -- UNSTABLE -- Built 2011.09.09 02:23pm";
 	public final static String		m_title					= "OPBM - Office Productivity Benchmark - " + m_version;
 }
