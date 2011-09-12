428c4eda9b2cbb4497ab0052f45c1db746376f4e
Rick C. Hodgin <rick@canalabs.com>
2011-09-11 22:14:13 -0500

    Recompile opbm.jar with fixes for File I/O and 7zip worklet duplication

--- java/opbm/src/opbm/Opbm.java
+++ java/opbm/src/opbm/Opbm.java
@@ -2921,6 +2921,6 @@ public final class Opbm extends	ModalApp
 
 	// Used for the build-date and time
 //	public final static String		m_version				= "Built 2011.08.22 05:19am";
-	public final static String		m_version				= "-- DEV BRANCH BUILD -- UNSTABLE -- Built 2011.09.09 10:29pm";
+	public final static String		m_version				= "-- DEV BRANCH BUILD -- UNSTABLE -- Built 2011.09.11 10:12pm";
 	public final static String		m_title					= "OPBM - Office Productivity Benchmark - " + m_version;
 }
